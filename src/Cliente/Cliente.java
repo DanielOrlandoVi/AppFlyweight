dd93819f5281bb75c852358c72ad9b43cfac85c7
YeyoDev99 <yeyoduglas2004@gmail.com>
2024-03-28 16:13:45 -0500

    codigo de barras random

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -5,12 +5,14 @@ import javax.swing.JOptionPane;
 import Contexto.Gaseosa;
 import FabricaFlyweight.FabricaTipoGaseosa;
 import Flyweight.TipoGaseosa;
+import java.util.Random;
+
 
 public class Cliente {
 
 
     public static void main(String[] args) {
-        
+        Random random = new Random();
         String gaseosas[] = {"Postobon Uva 100ml", "Colombiana 500ml"};
         
         while (true) {
@@ -27,6 +29,7 @@ public class Cliente {
             
             
             int cont = 0; //Contador para los codigos de barras
+            int codBarras = 0;
             switch (opcionGaseosa) {
                 case "Postobon Uva 100ml":
                     //Se verifica si la referencia esta en el hashmap, sino, se crea una vez, si esta, la devuelve sin crear nada mas
@@ -35,8 +38,11 @@ public class Cliente {
                     StringBuilder datosGaseosasP = new StringBuilder(); //Concatenamos los datos de las gaseosas para imprimirlos en pantalla
                     
                     for (int i = 0; i < numUnidades; i++) {
-                    cont = i;
-                    int codBarras = cont; // crea un codigo de barras unico por cada gaseosa
+                    cont = random.nextInt(1000);
+                    while (codBarras == cont){
+                        cont = random.nextInt(1000);
+                    }
+                    codBarras = cont; // crea un codigo de barras unico por cada gaseosa
                     //ahorro de memoria ya que tipoGaseosa solo se creo solo una vez
                     //Creamos las gaseosas particulares usando los datos generales a traves de tipoGaseosas(ahorro memoria)
                     Gaseosa gaseosa = new Gaseosa(codBarras, tipoGaseosa);
@@ -52,9 +58,12 @@ public class Cliente {
 
                     StringBuilder datosGaseosasC = new StringBuilder(); //Concatenamos los datos de las gaseosas para imprimirlos
                     for (int i = 0; i < numUnidades; i++) {
-                    cont = i;
-                    int codBarras = cont; // crea un codigo de barras unico por cada gaseosa
-                    //Creamos las gaseosas particulares usando los datos generales a traves de tipoGaseosas(ahorro memoria)
+                        cont = random.nextInt(1000);
+                        while (codBarras == cont){
+                            cont = random.nextInt(1000);
+                        }
+                        codBarras = cont; // crea un codigo de barras unico por cada gaseosa
+                        //Creamos las gaseosas particulares usando los datos generales a traves de tipoGaseosas(ahorro memoria)
                     Gaseosa gaseosa = new Gaseosa(codBarras, tipoGaseosa);
                     datosGaseosasC.append(gaseosa.mostrarDatos()).append("\n");
                     }
